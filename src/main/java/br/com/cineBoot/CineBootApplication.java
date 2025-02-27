9a22a5360896d2aeb4010b6b0868c3cc21b6d3a4
jose-matheusc <josematheus112@gmail.com>
2025-02-27 18:56:23 -0300

    primeiro commit

---
+++ src/main/java/br/com/cineBoot/CineBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.cineBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cineBoot/CineBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cineBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
